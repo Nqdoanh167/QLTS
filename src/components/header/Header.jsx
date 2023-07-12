a6ecc2c64c8ea0110a334cd4dcf2da11c4f175a0
Nqdoanh167 <nqdcntt2002@gmail.com>
2023-07-12 09:38:59 +0700

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,19 @@
+/** @format */
+
+import './header.scss';
+
+export default function Header() {
+   return (
+      <div className='headerWrap'>
+         <div className='header'>
+            <div className='headerText'>Trang chủ</div>
+            <div className='header-list'>
+               <div className='header-item'>1</div>
+               <div className='header-item'>2</div>
+               <div className='header-item'>3</div>
+               <div className='header-item'>4</div>
+            </div>
+         </div>
+      </div>
+   );
+}
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,52 @@
+/** @format */
+
+import './sidebar.scss';
+
+export default function Sidebar() {
+   return (
+      <div className='sidebarWrap'>
+         <div className='logo'>
+            <img src='./logo.png' alt='' className='logoImg' />
+         </div>
+         <hr />
+         <div className='list'>
+            <div className='item'>
+               <div className='icon'>
+                  <img src='./home.png' alt='' />
+               </div>
+               <p className='text'>Trang chủ</p>
+            </div>
+            <div className='item'>
+               <div className='icon'>
+                  <img src='./home.png' alt='' />
+               </div>
+               <p className='text'>Trang chủ</p>
+            </div>
+            <div className='item'>
+               <div className='icon'>
+                  <img src='./home.png' alt='' />
+               </div>
+               <p className='text'>Trang chủ</p>
+            </div>
+            <div className='item'>
+               <div className='icon'>
+                  <img src='./home.png' alt='' />
+               </div>
+               <p className='text'>Trang chủ</p>
+            </div>
+            <div className='item'>
+               <div className='icon'>
+                  <img src='./home.png' alt='' />
+               </div>
+               <p className='text'>Trang chủ</p>
+            </div>
+            <div className='item'>
+               <div className='icon'>
+                  <img src='./home.png' alt='' />
+               </div>
+               <p className='text'>Trang chủ</p>
+            </div>
+         </div>
+      </div>
+   );
+}
