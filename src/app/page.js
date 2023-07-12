5a5a1a959a68c042fbf56df5a636a6d059d61e36
Nqdoanh167 <nqdcntt2002@gmail.com>
2023-07-12 09:59:27 +0700

    hihi

--- src/app/page.js
+++ src/app/page.js
@@ -2,15 +2,50 @@
 
 import Image from 'next/image';
 import './page.scss';
-import 'styles/custom-bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 export default function Home() {
    return (
-      <div className='home'>
+      <div className='homes'>
          <div className='bannel'>
             <img src='./dl_app_pc2.png' alt='' className='img1' />
             <img src='./dl_app_pc1.png' alt='' className='img2' />
          </div>
          <div className='title'>Tổng quan</div>
+         <div className='khoi_lon'>
+            <div className='khoi_nho'>
+               <div className='top'>
+                  <div className='img'>
+                     <img src='./cp_icon.png' alt='' />
+                  </div>
+                  <p className='content'>Cấp phát</p>
+               </div>
+               <div className='bottom'>
+                  <p className='content'>0 Biên bản</p>
+               </div>
+            </div>
+            <div className='khoi_nho'>
+               <div className='top'>
+                  <div className='img'>
+                     <img src='./cp_icon.png' alt='' />
+                  </div>
+                  <p className='content'>Cấp phát</p>
+               </div>
+               <div className='bottom'>
+                  <p className='content'>0 Biên bản</p>
+               </div>
+            </div>
+            <div className='khoi_nho'>
+               <div className='top'>
+                  <div className='img'>
+                     <img src='./cp_icon.png' alt='' />
+                  </div>
+                  <p className='content'>Cấp phát</p>
+               </div>
+               <div className='bottom'>
+                  <p className='content'>0 Biên bản</p>
+               </div>
+            </div>
+         </div>
       </div>
    );
 }
